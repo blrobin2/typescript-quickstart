910666d871a4055624336af66ae84fced85202cd
Bennie <brobinson88@gmail.com>
2017-02-08 15:49:42 -0500

    React example

--- src/app.tsx
+++ src/app.tsx
@@ -1,3 +1,16 @@
-function test(thing:string): string {
-  return `Hey ${test}`;
+import * as React from 'react'
+import {render} from 'react-dom'
+
+const Hello = (props: { compiler: string, framework: string }) => {
+  return (
+    <div>
+      <div>{props.compiler}</div>
+      <div>{props.framework}</div>
+    </div>
+  )
 }
+
+render(
+  <Hello compiler="TypeScript" framework="React" />,
+  document.getElementById('root')
+)
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   entry: './src/app.tsx',
   output: {
-    path: './build',
+    path: '/build',
     filename: 'bundle.js'
   },
   resolve: {
